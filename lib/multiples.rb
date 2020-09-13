2d403122e07c9fa6f2b87290b58de3395ce4f970
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-09-13 13:30:52 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+
+# list all natural numbers below limit that are multiples of 3 or 5
+# collects correct multiples of natural numbers below limit
+def collect_multiples(limit)
+  multiples = []
+  (1...limit).each do |num|
+    multiples << num if (num % 3 == 0 || num % 5 == 0)
+  end
+  return multiples
+end
+
+# returns correct sum of multiples of 10
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+  # return multiples.sum
+  return multiples.inject(0){|sum,n| sum + n }
+end
